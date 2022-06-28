b3f4a4ee7346f025ed2418ebad9fa53cf0959826
junvely <junvely97@gmail.com>
2022-06-28 23:41:51 +0900

    simpleHabit-reactHook(before)

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./app.css";
-import App from "./app";
+// import App from "./app";
+import SimpleHabit from "./components/simpleHabit-reactHook";
 import "@fortawesome/fontawesome-free/js/all.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <SimpleHabit />
   </React.StrictMode>
 );
