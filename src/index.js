90e71054f9ad668cbbea55e761510163c7ba336a
junvely <junvely97@gmail.com>
2022-06-29 00:57:19 +0900

    simpleHabit-reactHook(after)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./app.css";
 // import App from "./app";
-import SimpleHabit from "./components/simpleHabit-reactHook";
+import SimpleHabit from "./components/simpleHabit";
 import "@fortawesome/fontawesome-free/js/all.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
